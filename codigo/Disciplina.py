dfd0a77ba8ca600199bb906af642855125cf5363
Damazu <davidias321123@gmail.com>
2024-08-20 23:50:50 -0300

    feat: criação atributos Disciplina

---
+++ codigo/Disciplina.py
@@ -0,0 +1,15 @@
+class Curso:
+    def __init__(self, nome, numCreditos, abertoMatricula):
+        self.nome = nome
+        self.creditos= numCreditos
+        self.abertoMatricula = abertoMatricula
+
+    def verificarNumAlunos(self):
+        print("")
+        
+    def alterarStatus(self):
+        print(f"{True}")
+
+    def associarProfessor(self):
+        print("")
+    
