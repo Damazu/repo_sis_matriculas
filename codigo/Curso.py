d0fbf326c3c7761ffa75839bb64f15aa5ae40b5b
Damazu <davidias321123@gmail.com>
2024-08-20 23:46:11 -0300

    feat: criar atributos de curso

---
+++ codigo/Curso.py
@@ -0,0 +1,13 @@
+class Curso:
+    def __init__(self, nomeCurso):
+        self.nomeCurso = nomeCurso
+
+    def incluirDisciplina(self):
+        print("")
+        
+    def cadastrarAluno(self):
+        print("")
+
+    def  removerAluno(self):
+        print("")
+    
