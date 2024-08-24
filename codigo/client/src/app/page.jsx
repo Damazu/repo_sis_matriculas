ea02a20022e19a7449bb8e09d03862f5deeb353e
Damazu <davidias321123@gmail.com>
2024-08-23 22:01:39 -0300

    fix: erro de exibição na interface

--- codigo/client/src/app/page.jsx
+++ codigo/client/src/app/page.jsx
@@ -1,8 +1,35 @@
-import React, {useEffect, useState} from 'react'
+'use client'
+import React from "react"
+import { useEffect, useState } from "react"
+
 
 function page() {
+
+const [message, setMessage] = useState("Loading")
+const [people, setPeople] = useState([])
+
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/home").then(
+      (response) => response.json()
+    ).then((data) =>{
+      setMessage(data.message)
+      setPeople(data.people)
+    }
+
+    )
+  })
+
   return (
-    <div>page</div>
+    <>
+    <div>{message}</div>
+    <div>{people.map((person, index)=>(
+        <div key={index}>
+          {person}
+        </div>
+    ))
+      }</div>
+    </>
   )
 }
 
