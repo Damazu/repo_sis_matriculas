b95988c4f5d32a7421525f2d2d10ec34089f8de4
Damazu <davidias321123@gmail.com>
2024-09-04 10:15:28 -0300

    feat : delete

--- codigo/client/src/app/PerfilAluno/page.jsx
+++ codigo/client/src/app/PerfilAluno/page.jsx
@@ -100,6 +100,40 @@ const PerfilAluno = () => {
       setNotification({ message: 'Erro ao desmatricular a disciplina. Tente novamente.', color: 'red' });
     });
   };
+  const handleDeletarAluno = () => {
+    if (!alunoSelecionado) return;
+
+    axios.delete(`http://localhost:8080/api/delete_aluno/${alunoSelecionado.idAluno}`)
+      .then(() => {
+        setNotification({ message: 'Aluno deletado com sucesso!', color: 'green' });
+        setAlunos(alunos.filter(aluno => aluno.idAluno !== alunoSelecionado.idAluno));
+        setAlunoSelecionado(null);
+        setDisciplinas([]);
+      })
+      .catch((error) => {
+        console.error('Erro ao deletar o aluno:', error);
+        setNotification({ message: 'Erro ao deletar o aluno. Tente novamente.', color: 'red' });
+      });
+  };
+
+  const handleAdicionarDisciplina = () => {
+    if (!disciplinaSelecionada) return;
+
+    axios.post(`http://localhost:8080/api/matricular_aluno`, {
+      idAluno: alunoSelecionado.idAluno,
+      disciplinas: [disciplinaSelecionada]
+    })
+    .then(() => {
+      setNotification({ message: 'Disciplina adicionada com sucesso!', color: 'green' });
+      setDisciplinas([...disciplinas, todasDisciplinas.find(d => d.idDisciplinas === parseInt(disciplinaSelecionada))]);
+      setOpened(false);
+      setDisciplinaSelecionada(null);
+    })
+    .catch((error) => {
+      console.error('Erro ao adicionar a disciplina:', error);
+      setNotification({ message: 'Erro ao adicionar a disciplina. Tente novamente.', color: 'red' });
+    });
+  };
 
   return (
     <Container style={{ marginTop: '30px', maxWidth: '800px' }}>
@@ -214,6 +248,9 @@ const PerfilAluno = () => {
                         )}
                       </tbody>
                     </Table>
+                    <Button color="red" onClick={handleDeletarAluno}>
+                    Deletar Aluno
+                  </Button>
                   </>
                 )}
 
